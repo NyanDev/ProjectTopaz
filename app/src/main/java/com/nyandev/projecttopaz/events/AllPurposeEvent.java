b4df73e4abe26e62b2393e5432c549457fd9f792
NyanDev <xuan-nghia.hoang@outlook.com>
2017-10-14 11:14:56 +0200

    Changed package name for releasing app

---
+++ app/src/main/java/com/nyandev/projecttopaz/events/AllPurposeEvent.java
@@ -0,0 +1,17 @@
+package com.nyandev.projecttopaz.events;
+
+/**
+ * Created by xuan- on 15/08/2017.
+ */
+
+public class AllPurposeEvent {
+    private final String message;
+
+    public AllPurposeEvent(String message) {
+        this.message = message;
+    }
+
+    public String getMessage(){
+        return message;
+    }
+}
---
+++ app/src/main/java/com/nyandev/projecttopaz/models/WeatherDay.java
@@ -0,0 +1,182 @@
+package com.nyandev.projecttopaz.models;
+
+import com.google.gson.annotations.SerializedName;
+
+import java.util.List;
+
+/**
+ * Created by Niiaou on 22/05/2017.
+ */
+
+public class WeatherDay {
+
+    @SerializedName("dt") private long dt;
+    @SerializedName("temp") private Temp temp;
+    @SerializedName("pressure") private double pressure;
+    @SerializedName("humidity") private double humidity;
+    @SerializedName("weather") private List<Weather> weather;
+    @SerializedName("speed") private double speed;
+    @SerializedName("deg") private double deg;
+    @SerializedName("clouds") private int clouds;
+
+    public class Temp{
+        @SerializedName("day") private double day;
+        @SerializedName("min") private double min;
+        @SerializedName("max") private double max;
+        @SerializedName("night") private double night;
+        @SerializedName("eve") private double eve;
+        @SerializedName("morn") private double morn;
+
+        public double getDay() {
+            return day;
+        }
+
+        public void setDay(double day) {
+            this.day = day;
+        }
+
+        public double getMin() {
+            return min;
+        }
+
+        public void setMin(double min) {
+            this.min = min;
+        }
+
+        public double getMax() {
+            return max;
+        }
+
+        public void setMax(double max) {
+            this.max = max;
+        }
+
+        public double getNight() {
+            return night;
+        }
+
+        public void setNight(double night) {
+            this.night = night;
+        }
+
+        public double getEve() {
+            return eve;
+        }
+
+        public void setEve(double eve) {
+            this.eve = eve;
+        }
+
+        public double getMorn() {
+            return morn;
+        }
+
+        public void setMorn(double morn) {
+            this.morn = morn;
+        }
+    }
+
+    public class Weather{
+        @SerializedName("id") private int id;
+        @SerializedName("main") private String main;
+        @SerializedName("description") private String description;
+        @SerializedName("icon") private String icon;
+
+        public int getId() {
+            return id;
+        }
+
+        public void setId(int id) {
+            this.id = id;
+        }
+
+        public String getMain() {
+            return main;
+        }
+
+        public void setMain(String main) {
+            this.main = main;
+        }
+
+        public String getDescription() {
+            return description;
+        }
+
+        public void setDescription(String description) {
+            this.description = description;
+        }
+
+        public String getIcon() {
+            return icon;
+        }
+
+        public void setIcon(String icon) {
+            this.icon = icon;
+        }
+    }
+
+    public long getDt() {
+        return dt;
+    }
+
+    public void setDt(long dt) {
+        this.dt = dt;
+    }
+
+    public Temp getTemp() {
+        return temp;
+    }
+
+    public void setTemp(Temp temp) {
+        this.temp = temp;
+    }
+
+    public double getPressure() {
+        return pressure;
+    }
+
+    public void setPressure(double pressure) {
+        this.pressure = pressure;
+    }
+
+    public double getHumidity() {
+        return humidity;
+    }
+
+    public void setHumidity(double humidity) {
+        this.humidity = humidity;
+    }
+
+    public List<Weather> getWeather() {
+        return weather;
+    }
+
+    public void setWeather(List<Weather> weather) {
+        this.weather = weather;
+    }
+
+    public double getSpeed() {
+        return speed;
+    }
+
+    public void setSpeed(double speed) {
+        this.speed = speed;
+    }
+
+    public double getDeg() {
+        return deg;
+    }
+
+    public void setDeg(double deg) {
+        this.deg = deg;
+    }
+
+    public int getClouds() {
+        return clouds;
+    }
+
+    public void setClouds(int clouds) {
+        this.clouds = clouds;
+    }
+
+}
