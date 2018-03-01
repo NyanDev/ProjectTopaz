77ce0cdd70c6406cc6b933d2167a102a6f1777d2
NyanDev <xuan-nghia.hoang@outlook.com>
2018-03-01 19:10:24 +0100

    Refactoring into data package

---
+++ app/src/main/java/com/nyandev/projecttopaz/data/events/WeatherForecastEvent.java
@@ -0,0 +1,21 @@
+package com.nyandev.projecttopaz.data.events;
+
+/**
+ * Created by xuan- on 27/08/2017.
+ */
+
+public class WeatherForecastEvent {
+    private final int position;
+    private final String location;
+
+    public WeatherForecastEvent(int position, String location) {
+        this.position = position;
+        this.location = location;
+    }
+
+    public int getPosition(){
+        return position;
+    }
+
+    public String getLocation(){ return location; }
+}
