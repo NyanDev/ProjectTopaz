93be91b6a2934798124238c8556f8e70665a8724
NyanDev <xuan-nghia.hoang@outlook.com>
2018-03-01 15:52:34 +0100

    added getStartIntent function

--- app/src/main/java/com/nyandev/projecttopaz/ui/settings/SettingsActivity.java
+++ app/src/main/java/com/nyandev/projecttopaz/ui/settings/SettingsActivity.java
@@ -1,16 +1,25 @@
 package com.nyandev.projecttopaz.ui.settings;
 
+import android.content.Context;
+import android.content.Intent;
 import android.content.SharedPreferences;
 import android.os.Bundle;
 import android.preference.PreferenceManager;
 import android.support.annotation.Nullable;
 import android.support.v7.app.AppCompatActivity;
+import android.widget.Toast;
 
 /**
  * Created by xuan- on 01/03/2018.
  */
 
 public class SettingsActivity extends AppCompatActivity{
+
+    public static Intent getStartIntent(Context context){
+        Intent intent = new Intent(context, SettingsActivity.class);
+        return intent;
+    }
+
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -20,4 +29,9 @@ public class SettingsActivity extends AppCompatActivity{
                 .commit();
     }
 
+    @Override
+    public void onBackPressed() {
+        super.onBackPressed();
+        Toast.makeText(this, "BACK", Toast.LENGTH_SHORT).show();
+    }
 }
